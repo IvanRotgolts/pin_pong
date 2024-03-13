0be2989ec6a9dc4c616ab6e7aa27199e29b959b6
Inter <rotgolts.ivan@gmail.com>
2024-03-13 18:59:39 +0300

    добавили движение ракетки

--- constants.py
+++ constants.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Pin Pong"
+BASE_PATH = str(Path(__file__).resolve().parent)
+BAR_SPEED_X = 3
--- pin_pong.py
+++ pin_pong.py
@@ -2,6 +2,8 @@ import arcade
 from constants import *
 
 class Game(arcade.Window):
+    """главный игровой класс
+    здесь мы объединяем все части нашей игры"""
     def __init__(self, width, height, title) : 
         #функция super вызывает конструктор класса родителя и передает ему аргументы(значения)
         super().__init__(width, height, title) 
@@ -9,21 +11,44 @@ class Game(arcade.Window):
         #Создаем объект шарик
         self.ball = Ball(300, 300, 3, 2)
 
+        self.bar = Bar(300, 50, 3)
+
     def on_draw(self):
         #делает отрисовку
         self.background_color = (0, 128, 128)
         self.clear()
 
         self.ball.draw()
+
+        self.bar.draw()
     
     def update(self, delta_time: float):
         #обновляет положение
         self.ball.update()
 
+        self.bar.update()
+
+        print(arcade.check_for_collision(self.ball, self.bar))
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.LEFT:
+            self.bar.change_x = -BAR_SPEED_X
+            
+        if symbol == arcade.key.RIGHT:
+            self.bar.change_x = BAR_SPEED_X
+
+    def on_key_release(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.LEFT:
+            self.bar.change_x = 0
+
+        if symbol == arcade.key.RIGHT:
+            self.bar.change_x = 0
+
+
 class Ball(arcade.Sprite):
     #Описываю шарик
     def __init__(self, x, y, speed_x, speed_y):
-        super().__init__(filename = "C:\\MyPrograms\\arcade\\pin_pong\\ball.png", scale = 0.2)
+        super().__init__(filename = BASE_PATH + "\\ball.png", scale = 0.15)
         #устанавливаем атрибутам положение и скорость
         self.center_x = x
         self.center_y = y
@@ -50,8 +75,9 @@ class Ball(arcade.Sprite):
             self.change_y *= -1
 
 class Bar(arcade.Sprite):
+    """описывает объект ракетки"""
     def __init__(self, x, y, speed):
-        super().__init__(filename = "C:\\MyPrograms\\arcade\\pin_pong\\bar.png", scale = 0.2)
+        super().__init__(filename = BASE_PATH + "\\bar.png", scale = 0.1)
         self.center_x = x
         self.center_y = y
         self.change_x = speed
